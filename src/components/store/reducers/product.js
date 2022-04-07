3bfd99c2eb744a01b6ed4022f6f904d402988f89
Artemiza Rocha <miza-pma@hotmail.com>
2022-04-06 21:55:43 -0300

    Conclusão

---
+++ src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+  {
+    id_product: 1,
+    category: "Tv e Vídeos",
+    fk_idcategorys: 1,
+    name_product: "Chromecast",
+    price: 359.00,
+    image: "./images/produtos/chromecast.jpg",
+    id_categorys: 1,
+    name_categorys: "Tv e Vídeos",
+    selected: false
+  },
+  {
+    id_product: 2,
+    category: "Tv e Vídeos",
+    fk_idcategorys: 1,
+    name_product: "Smart TV Samsung",
+    price: 2599.00,
+    image: "./images/produtos/tv.jpg",
+    id_categorys: 1,
+    name_categorys: "Tv e Vídeos",
+    selected: false
+  },
+  {
+    id_product: 9,
+    category: "Eletroportáteis",
+    fk_idcategorys: 2,
+    name_product: "Panela de Pressão ",
+    price: 119.90,
+    image: "./images/produtos/panela de pressão.jpg",
+    id_categorys: 2,
+    name_categorys: "Eletroportáteis",
+    selected: false
+  },
+  {
+    id_product: 4,
+    category: "Eletroportáteis",
+    fk_idcategorys: 2,
+    name_product: "Fritadeira Elétrica",
+    price: 349.99,
+    image: "./images/produtos/fritadeira.jpg",
+    id_categorys: 2,
+    name_categorys: "Eletroportáteis",
+    selected: false
+   },
+   {
+    id_product: 10,
+    category: "Eletroportáteis",
+    fk_idcategorys: 2,
+    name_product: "Liquidificador Mondial",
+    price: 99.99,
+    image: "./images/produtos/liquidificador.jpg",
+    id_categorys: 2,
+    name_categorys: "Eletroportáteis",
+    selected: false
+    },
+    {
+      id_product: 11,
+      category: "Movéis e Decoração",
+      fk_idcategorys: 3,
+      name_product: "Cama Box Casal",
+      price: 899.90,
+      image: "./images/produtos/cama.jpg",
+      id_categorys: 3,
+      name_categorys: "Movéis e Decoração",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "Movéis e Decoração",
+      fk_idcategorys: 3,
+      name_product: "Quarda Roupa",
+      price: 529.90,
+      image: "./images/produtos/guarda roupa.jpg",
+      id_categorys: 3,
+      name_categorys: "Movéis e Decoração",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "Movéis e Decoração",
+      fk_idcategorys: 3,
+      name_product: "Sofá",
+      price: 899.99,
+      image: "./images/produtos/sofa.jpg",
+      id_categorys: 3,
+      name_categorys: "Movéis e Decoração",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "Eletrodoméstico",
+      fk_idcategorys: 4,
+      name_product: "Geladeira",
+      price: 4277.90,
+      image: "./images/produtos/galadeira.jpg",
+      id_categorys: 4,
+      name_categorys: "Eletrodoméstico",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "Ar e Ventilação",
+      fk_idcategorys: 4,
+      name_product: "Ventilador",
+      price: 139.90,
+      image: "./images/produtos/ventilador.jpg",
+      id_categorys: 4,
+      name_categorys: "Ar e Ventilação",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "Celulares",
+      fk_idcategorys: 5,
+      name_product: "Iphone 12",
+      price: 5499.90,
+      image: "./images/produtos/iphone.jpg",
+      id_categorys: 5,
+      name_categorys: "Celulares",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "Informática",
+      fk_idcategorys: 5,
+      name_product: "Notebook Compaq" ,
+      price: 2659.99,
+      image: "./images/produtos/notebook.jpg",
+      id_categorys: 5,
+      name_categorys: "Informática",
+      selected: false
+    },
+];
+
+export default productsList;
